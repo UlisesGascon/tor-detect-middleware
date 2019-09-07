a0c55af111e22fb71f49bffac160620d04e686da
ulises Gascon <ulisesgascongonzalez@gmail.com>
2019-09-07 10:12:45 +0200

    Added Tests Matrix

---
+++ __tests__/index.test.js
@@ -0,0 +1,17 @@
+describe('Tor Detect Middelware behaviour', () => {
+  test.skip('should recognize a TOR IP origin', () => { })
+
+  test.skip('Should recognize a surface IP origin', () => {})
+
+  test.skip('Should not wait until there is available data in the store', () => {})
+
+  test.skip('Should wait until there is available data in the store in strict Mode', () => {})
+
+  test.skip('Should not stop the server if the onioono service is down', () => {})
+
+  test.skip('Should stop th server if the onioono service is down in strict Mode', () => {})
+
+  test.skip('Should refresh the store every hour by default', () => {})
+
+  test.skip('Should refresh the store with a custom CRON Job', () => {})
+})
