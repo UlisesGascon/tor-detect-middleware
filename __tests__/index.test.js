730ffc44f8389de8a641ebcf68a5e383e5b35ff8
ulises Gascon <ulisesgascongonzalez@gmail.com>
2019-09-07 19:16:59 +0200

    WIP.Tor Detect Middelware DEFAULT behaviour
    - Added  feature: recognize a TOR IP origin
    - Added feature: recognize a surface IP origin
    - Added LOWdb dependency
    - Added store module with basic functionality

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,36 @@
+const torUserHandler = require('../lib')
+const store = require('../lib/store')
+
+const populateDB = nodes => {
+  store.setNodes(nodes)
+  expect(store.getNodes()).toEqual(nodes)
+}
+
+const mockResponse = () => {
+  const res = {}
+  return res
+}
+
+const mockRequest = (ip) => {
+  const req = {
+    headers: {},
+    connection: {},
+    send: jest.fn()
+  }
+  req.connection.remoteAddress = ip
+  return req
+}
+
+beforeEach((done) => {
+  store.clean()
+  done()
+})
+
+afterAll((done) => {
+  store.clean()
+  done()
+})
+
 describe.skip('Tor Detect Middelware strict mode behaviour', () => {
   test('Should stop th server if the onioono service is down in strict Mode', () => {})
   test('Should wait until there is available data in the store in strict Mode', () => {})
@@ -15,11 +48,31 @@ describe.skip('Tor Detect Middelware REDIRECT behaviour', () => {
   test('should not redirect a TOR user', () => {})
 })
 
-describe.skip('Tor Detect Middelware DEFAULT behaviour', () => {
-  test('should recognize a TOR IP origin', () => {})
-  test('Should recognize a surface IP origin', () => {})
-  test('Should not wait until there is available data in the store', () => {})
-  test('Should not stop the server if the onioono service is down', () => {})
+describe('Tor Detect Middelware DEFAULT behaviour', () => {
+  test('should recognize a TOR IP origin', (done) => {
+    populateDB(['IP-TOR', 'IP-TOR-2'])
+    const req = mockRequest('IP-TOR')
+    const res = mockResponse()
+
+    torUserHandler(req, res, () => {
+      expect(req.isTorUser).toEqual(true)
+      done()
+    })
+  })
+
+  test('Should recognize a surface IP origin', (done) => {
+    populateDB(['IP-TOR', 'IP-TOR-2'])
+    const req = mockRequest('IP-SURFACE')
+    const res = mockResponse()
+
+    torUserHandler(req, res, () => {
+      expect(req.isTorUser).toEqual(false)
+      done()
+    })
+  })
+
+  test.skip('Should not wait until there is available data in the store', () => {})
+  test.skip('Should not stop the server if the onioono service is down', () => {})
 })
 
 describe.skip('Tor Detect Middelware PURGE behaviour', () => {
