f23d9cbb0118d128bd76685c231b03570b28e065
ulises Gascon <ulisesgascongonzalez@gmail.com>
2019-09-07 10:56:48 +0200

    Test Matrix refactored

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,25 +1,28 @@
-describe('Tor Detect Middelware behaviour', () => {
-  test.skip('should recognize a TOR IP origin', () => { })
+describe.skip('Tor Detect Middelware strict mode behaviour', () => {
+  test('Should stop th server if the onioono service is down in strict Mode', () => {})
+  test('Should wait until there is available data in the store in strict Mode', () => {})
+})
 
-  test.skip('Should recognize a surface IP origin', () => {})
+describe.skip('Tor Detect Middelware CRON behaviour', () => {
+  test('Should refresh the store every hour by default', () => {})
+  test('Should refresh the store with a custom CRON Job', () => {})
+})
 
-  test.skip('should recognize a TOR IP origin and redirect', () => { })
+describe.skip('Tor Detect Middelware REDIRECT behaviour', () => {
+  test('should redirect a surface user', () => {})
+  test('should not redirect a surface user', () => {})
+  test('should redirect a TOR user', () => {})
+  test('should not redirect a TOR user', () => {})
+})
 
-  test.skip('Should recognize a surface IP origin and redirect', () => {})
+describe.skip('Tor Detect Middelware DEFAULT behaviour', () => {
+  test('should recognize a TOR IP origin', () => {})
+  test('Should recognize a surface IP origin', () => {})
+  test('Should not wait until there is available data in the store', () => {})
+  test('Should not stop the server if the onioono service is down', () => {})
+})
 
-  test.skip('Should not wait until there is available data in the store', () => {})
-
-  test.skip('Should wait until there is available data in the store in strict Mode', () => {})
-
-  test.skip('Should not stop the server if the onioono service is down', () => {})
-
-  test.skip('Should stop th server if the onioono service is down in strict Mode', () => {})
-
-  test.skip('Should refresh the store every hour by default', () => {})
-
-  test.skip('Should refresh the store with a custom CRON Job', () => {})
-
-  test.skip('Should purge the IP list', () => {})
-
-  test.skip('Should purge the IP list only at the startup', () => {})
+describe.skip('Tor Detect Middelware PURGE behaviour', () => {
+  test('Should purge the IP list', () => {})
+  test('Should purge the IP list only at the startup', () => {})
 })
