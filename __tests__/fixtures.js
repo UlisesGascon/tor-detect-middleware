0a7e19ef3a1e7546fae0b0871a4781c62c03f921
Ulises Gascon <ulises.gascon@guidesmiths.com>
2020-05-31 14:13:29 +0200

    updated fixtures for exit nodes

--- __tests__/fixtures.js
+++ __tests__/fixtures.js
@@ -1,15 +1,20 @@
 const ipSurface = '79.189.12.130'
 const ipTor = '244.242.84.73'
-const torIpList = [ipTor, '9.58.138.153', '180.206.23.192']
+const ipTor2 = '180.206.23.192'
+const exitTorList = [ipTor, ipTor2]
 const nytUrl = 'https://www.nytimes.com'
 const nytTorUrl = 'https://www.nytimes3xbfgragh.onion/'
 const emptyDbMsg = 'Tor Database is empty'
 const defaultMs = 3600000
 const simplifiedResponse = {
   relays: [{
-    or_addresses: [ipTor]
-  }, { or_addresses: ['9.58.138.153', '180.206.23.192'] }
+    or_addresses: [ipTor],
+    flags: ['Exit']
+  }, {
+    or_addresses: [ipTor2],
+    flags: ['Exit']
+  }, { or_addresses: ['9.58.138.153'], flags: [] }
   ]
 }
 
-module.exports = { simplifiedResponse, ipSurface, ipTor, torIpList, nytUrl, nytTorUrl, emptyDbMsg, defaultMs }
+module.exports = { simplifiedResponse, ipSurface, exitTorList, ipTor, nytUrl, nytTorUrl, emptyDbMsg, defaultMs }
